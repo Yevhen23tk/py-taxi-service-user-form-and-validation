746ba416eb5bfeb1774bfaa35d53bc5f771a5b4b
Yevhen Tkachuk <yevhen23tk@gmail.com>
2024-10-27 12:24:32 +0200

    Solution01

--- taxi/validators.py
+++ taxi/validators.py
@@ -5,12 +5,18 @@ from django.core.exceptions import ValidationError
 
 def validate_license_number(license_number):
     if len(license_number) != 8:
-        raise ValidationError("License number must be exactly 8 characters long.")
+        raise ValidationError(
+            "License number must be exactly 8 characters long."
+        )
 
     if not re.match(r"^[A-Z]{3}", license_number):
-        raise ValidationError("The first 3 characters must be uppercase letters.")
+        raise ValidationError(
+            "The first 3 characters must be uppercase letters."
+        )
 
     if not re.match(r"^[A-Z]{3}[0-9]{5}$", license_number):
-        raise ValidationError("The last 5 characters must be digits.")
+        raise ValidationError(
+            "The last 5 characters must be digits."
+        )
 
     return license_number
